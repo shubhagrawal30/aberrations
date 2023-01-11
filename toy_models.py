2e9f9f018eda06ba441a84f1a7cb876500f4f711
shubhagrawal30 <shubhagrawal30@gmail.com>
2023-01-10 21:38:11 -0800

    add mike paper plots

--- toy_models.py
+++ toy_models.py
@@ -16,22 +16,32 @@ from pathlib import Path
 print("setting up")
 
 # angular range and density of psfs
-RA_range = (-4, 4)
-Dec_range = (-4, 4)
-num_den = 100
+rad = 1.2
+RA_range = (-rad, rad)
+Dec_range = (-rad, rad)
+num_den = 10000
 
 # Zernike polynomials
-d = np.vectorize(lambda x, y: 1e-1)
-s = np.vectorize(lambda x, y: 1e-2)
+d = np.vectorize(lambda x, y: -0.006 + np.real((0.009 - 0.003j) * (x - 1j * y) / rad))
+s = np.vectorize(lambda x, y: 0)
 
-a_real, a_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: np.sqrt(x**2+y**2) * 3e-3) #
-a = np.vectorize(lambda x, y: a_real(x, y) + a_imag(x, y) * 1j)
+a = np.vectorize(lambda x, y: 0.014 - 0.011j + (0.001 - 0.002j) * (x + 1j*y))
+a_real, a_imag = np.vectorize(lambda x, y: np.real(a(x, y))), np.vectorize(lambda x, y: np.imag(a(x, y)))
 
-c_real, c_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: np.sqrt(x**2+y**2) * 3e-3)
+c_real, c_imag = np.vectorize(lambda x, y: -0.039), np.vectorize(lambda x, y: -0.010)
 c = np.vectorize(lambda x, y: c_real(x, y) + c_imag(x, y) * 1j)
 
+# d = np.vectorize(lambda x, y: 1e-1)
+# s = np.vectorize(lambda x, y: 1e-2)
+
+# a_real, a_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: np.sqrt(x**2+y**2) * 3e-3) #
+# a = np.vectorize(lambda x, y: a_real(x, y) + a_imag(x, y) * 1j)
+
+# c_real, c_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: np.sqrt(x**2+y**2) * 3e-3)
+# c = np.vectorize(lambda x, y: c_real(x, y) + c_imag(x, y) * 1j)
+
 # saving stuff
-subfolder = "20230109_radial_imag/"
+subfolder = "20230110_mike_paper/"
 # subfolder = "20230109_constant/"
 out_dir = f"./out/{subfolder}"
 plot_dir = f"./figs/{subfolder}"
@@ -46,7 +56,7 @@ labels = [r"$d$", r"$\Re(a)$", r"$\Im(a) \sim \sqrt{x^2+y^2}$", r"$s$", r"$\Re(c
           r"$\Re(Q)$", r"$\Im(Q)$", r"S"]
 # labels = [r"$d$", r"$\Re(a)$", r"$\Im(a)$", r"$s$", r"$\Re(c)$", r"$\Im(c)$", \
           # r"$\Re(Q)$", r"$\Im(Q)$", r"S"]
-thin_gal = 2
+thin_gal = 10
 
 # 3pt stuff
 rmin = 1
