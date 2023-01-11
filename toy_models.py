f6d6d4fec6c27dd5a72b1b7deb00ae7924d59b41
shubhagrawal30 <shubhagrawal30@gmail.com>
2023-01-10 22:23:39 -0800

    add mike paper plots with high n

--- toy_models.py
+++ toy_models.py
@@ -19,32 +19,33 @@ print("setting up")
 rad = 1.2
 RA_range = (-rad, rad)
 Dec_range = (-rad, rad)
-num_den = 10000
+num_den = 100000
 
 # Zernike polynomials
 
 # mike paper
-# d = np.vectorize(lambda x, y: -0.006 + np.real((0.009 - 0.003j) * (x - 1j * y) / rad))
-# s = np.vectorize(lambda x, y: 0)
+d = np.vectorize(lambda x, y: -0.006 + np.real((0.009 - 0.003j) * (x - 1j * y) / rad))
+s = np.vectorize(lambda x, y: 0)
 
-# a = np.vectorize(lambda x, y: 0.014 - 0.011j + (0.001 - 0.002j) * (x + 1j*y))
-# a_real, a_imag = np.vectorize(lambda x, y: np.real(a(x, y))), np.vectorize(lambda x, y: np.imag(a(x, y)))
+a = np.vectorize(lambda x, y: 0.014 - 0.011j + (0.001 - 0.002j) * (x + 1j*y) / rad)
+a_real, a_imag = np.vectorize(lambda x, y: np.real(a(x, y))), np.vectorize(lambda x, y: np.imag(a(x, y)))
 
-# c_real, c_imag = np.vectorize(lambda x, y: -0.039), np.vectorize(lambda x, y: -0.010)
-# c = np.vectorize(lambda x, y: c_real(x, y) + c_imag(x, y) * 1j)
-
-d = np.vectorize(lambda x, y: 1e-1)
-s = np.vectorize(lambda x, y: 1e-2)
-
-a_real, a_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: 3e-3) #np.sqrt(x**2+y**2) * 3e-3) #
-a = np.vectorize(lambda x, y: a_real(x, y) + a_imag(x, y) * 1j)
-
-c_real, c_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: 3e-3) #np.sqrt(x**2+y**2) * 3e-3)
+c_real, c_imag = np.vectorize(lambda x, y: -0.039), np.vectorize(lambda x, y: -0.010)
 c = np.vectorize(lambda x, y: c_real(x, y) + c_imag(x, y) * 1j)
 
+# d = np.vectorize(lambda x, y: 1e-1)
+# s = np.vectorize(lambda x, y: 1e-2)
+
+# a_real, a_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: np.sqrt(x**2+y**2) * 3e-3) #
+# a = np.vectorize(lambda x, y: a_real(x, y) + a_imag(x, y) * 1j)
+
+# c_real, c_imag = np.vectorize(lambda x, y: 1e-2), np.vectorize(lambda x, y: np.sqrt(x**2+y**2) * 3e-3)
+# c = np.vectorize(lambda x, y: c_real(x, y) + c_imag(x, y) * 1j)
+
 # saving stuff
-subfolder = "20230110_constant/"
-# subfolder = "20230109_constant/"
+# subfolder = "20230110_constant/"
+subfolder = "20230111_mike_paper/"
+# subfolder = "20230110_radial_imag/"
 out_dir = f"./out/{subfolder}"
 plot_dir = f"./figs/{subfolder}"
 Path(out_dir).mkdir(parents=True, exist_ok=True)
@@ -54,11 +55,12 @@ Path(plot_dir).mkdir(parents=True, exist_ok=True)
 coeffs = [d, a_real, a_imag, s, c_real, c_imag, \
          np.vectorize(lambda x, y : np.real(Q(x, y))), \
           np.vectorize(lambda x, y : np.imag(Q(x, y))), lambda x, y : S(x, y)]
-labels = [r"$d$", r"$\Re(a)$", r"$\Im(a) \sim \sqrt{x^2+y^2}$", r"$s$", r"$\Re(c)$", r"$\Im(c)\sim \sqrt{x^2+y^2}$", \
+# labels = [r"$d$", r"$\Re(a)$", r"$\Im(a) \sim \sqrt{x^2+y^2}$", r"$s$", r"$\Re(c)$", r"$\Im(c)\sim \sqrt{x^2+y^2}$", \
+#           r"$\Re(Q)$", r"$\Im(Q)$", r"S"]
+labels = [r"$d$", r"$\Re(a)$", r"$\Im(a)$", r"$s$", r"$\Re(c)$", r"$\Im(c)$", \
           r"$\Re(Q)$", r"$\Im(Q)$", r"S"]
-# labels = [r"$d$", r"$\Re(a)$", r"$\Im(a)$", r"$s$", r"$\Re(c)$", r"$\Im(c)$", \
-          # r"$\Re(Q)$", r"$\Im(Q)$", r"S"]
-thin_gal = 100
+thin_gal = 1000
+alpha=0.5
 
 # 3pt stuff
 rmin = 1
@@ -142,7 +144,7 @@ for ra, de, a_ep, b_ep, phi, gam in rand_coords[::thin_gal]:
     ra, de, a_ep, b_ep, phi = np.real([ra, de, a_ep, b_ep, phi])
     # Create an ellipse patch with a given size centered at the RA, Dec position
     plt.plot(*getLineParameters(ra, de, gam), color="black", lw=2)
-    ellipse = Ellipse(xy=(ra, de), width=a_ep, height=b_ep, angle=np.degrees(phi), alpha=0.2)
+    ellipse = Ellipse(xy=(ra, de), width=a_ep, height=b_ep, angle=np.degrees(phi), alpha=alpha)
     # Add the ellipse patch to the axes
     ax.add_patch(ellipse)
 
